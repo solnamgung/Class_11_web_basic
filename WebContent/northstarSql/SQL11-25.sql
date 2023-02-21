8853501b415b3f9e4418023ff737490944ee97ec
sol namgung <nandaras0103@gmail.com>
2023-02-21 23:12:58 +0900

    up to 17

--- WebContent/northstarSql/SQL11-25.sql
+++ WebContent/northstarSql/SQL11-25.sql
@@ -94,9 +94,165 @@
 				o.ITEM_CODE = b.ITEM_CODE 
       GROUP BY 
       			b.ITEM_CODE,
-      			b.ITEM_NAME ;
+      			b.ITEM_NAME
+      	HAVING
+      			
+       ORDER BY
+       			판매수;
+       			
+       			SELECT I.ITEM_CODE, SUM(O.QTY) AS TOTAL_QTY
+				FROM EXAM.BURGER_ITEM I
+				JOIN EXAM.BURGER_ORD_ITEM O
+				ON I.ITEM_CODE = O.ITEM_CODE
+				WHERE I.SET_YN = 'N' AND O.CANCEL_YN = 'N' AND O.ORD_CODE IN (
+				  SELECT ORD_CODE FROM EXAM.BURGER_ORD WHERE ORD_DATE BETWEEN '20170301' AND '20170331'
+				)
+				GROUP BY I.ITEM_CODE
+				ORDER BY TOTAL_QTY DESC;
+				
+				
+	//16
+			
+			SELECT 
+		  ITEM_NAME,
+		  SUM(
+		    CASE 
+		      WHEN SUBSTR(ORD_DATE, 1, 6) = '201701' THEN AMOUNT 
+		      ELSE 0 
+		    END
+		  ) AS JAN,
+		  SUM(
+		    CASE 
+		      WHEN SUBSTR(ORD_DATE, 1, 6) = '201702' THEN AMOUNT 
+		      ELSE 0 
+		    END
+		  ) AS FEB,
+		  SUM(
+		    CASE 
+		      WHEN SUBSTR(ORD_DATE, 1, 6) = '201703' THEN AMOUNT 
+		      ELSE 0 
+		    END
+		  ) AS MAR,
+		  SUM(
+		    CASE 
+		      WHEN SUBSTR(ORD_DATE, 1, 6) = '201704' THEN AMOUNT 
+		      ELSE 0 
+		    END
+		  ) AS APR,
+		  SUM(
+		    CASE 
+		      WHEN SUBSTR(ORD_DATE, 1, 6) = '201705' THEN AMOUNT 
+		      ELSE 0 
+		    END
+		  ) AS MAY,
+		  SUM(
+		    CASE 
+		      WHEN SUBSTR(ORD_DATE, 1, 6) = '201706' THEN AMOUNT 
+		      ELSE 0 
+		    END
+		  ) AS JUN,
+		  SUM(
+		    CASE 
+		      WHEN SUBSTR(ORD_DATE, 1, 6) = '201707' THEN AMOUNT 
+		      ELSE 0 
+		    END
+		  ) AS JUL,
+		  SUM(
+		    CASE 
+		      WHEN SUBSTR(ORD_DATE, 1, 6) = '201708' THEN AMOUNT 
+		      ELSE 0 
+		    END
+		  ) AS AUG,
+		  SUM(
+		    CASE 
+		      WHEN SUBSTR(ORD_DATE, 1, 6) = '201709' THEN AMOUNT 
+		      ELSE 0 
+		    END
+		  ) AS SEP,
+		  SUM(
+		    CASE 
+		      WHEN SUBSTR(ORD_DATE, 1, 6) = '201710' THEN AMOUNT 
+		      ELSE 0 
+		    END
+		  ) AS OCT,
+		  SUM(
+		    CASE 
+		      WHEN SUBSTR(ORD_DATE, 1, 6) = '201711' THEN AMOUNT 
+		      ELSE 0 
+		    END
+		  ) AS NOV,
+		  SUM(
+		    CASE 
+		      WHEN SUBSTR(ORD_DATE, 1, 6) = '201712' THEN AMOUNT 
+		      ELSE 0 
+		    END
+		  ) AS DEC
+		FROM 
+		  BURGER_ORD_ITEM 
+		  JOIN 
+		  BURGER_ORD 
+		  ON
+		  BURGER_ORD.ORD_CODE = BURGER_ORD_ITEM.ORD_CODE
+		  JOIN
+		  BURGER_ITEM 
+		  ON BURGER_ORD_ITEM.ITEM_CODE = BURGER_ITEM.ITEM_CODE 
+		WHERE 
+		  SUBSTR(ORD_DATE, 1, 4) = '2017' 
+	 GROUP BY 
+  			GROUPING SETS ((ITEM_NAME), ())
+		  
+		// GROUP BY 
+  			ROLLUP(ITEM_NAME);
   				
-  				
+  		
+		  //17 시간대별 매출을 구하라 
+				  
+				  SELECT
+			  STORE_ADDR  AS 지점,
+			  EMP_NAME	  AS 사원,
+		  SUM(
+		    CASE
+		      WHEN TO_CHAR(ORD_TIME, 'HH24') BETWEEN '00' AND '05' THEN BURGER_ORD_ITEM.AMOUNT
+		      ELSE 0
+		    END
+		  ) AS "00~06",
+		  SUM(
+		    CASE
+		      WHEN TO_CHAR(ORD_TIME, 'HH24') BETWEEN '06' AND '12' THEN BURGER_ORD_ITEM.AMOUNT
+		      ELSE 0
+		    END
+		  ) AS "06~13",
+		  SUM(
+		    CASE
+		      WHEN TO_CHAR(ORD_TIME, 'HH24') BETWEEN '13' AND '19' THEN BURGER_ORD_ITEM.AMOUNT
+		      ELSE 0
+		    END
+		  ) AS "13~20",
+		  SUM(
+		    CASE
+		      WHEN TO_CHAR(ORD_TIME, 'HH24') BETWEEN '20' AND '23' THEN BURGER_ORD_ITEM.AMOUNT
+		      ELSE 0
+		    END
+		  ) AS "20~24"
+		FROM
+		     BURGER_STORE 
+		JOIN 
+			 BURGER_EMP 
+		  ON 
+		  	 BURGER_EMP.STORE_CODE = BURGER_STORE.STORE_CODE
+		JOIN
+			 BURGER_ORD 
+		  ON
+		  	 BURGER_ORD.STORE_CODE = BURGER_STORE.STORE_CODE AND BURGER_ORD.EMP_CODE = BURGER_EMP.EMP_CODE 
+		JOIN
+			 BURGER_ORD_ITEM
+		  ON
+			 BURGER_ORD.ORD_CODE = BURGER_ORD_ITEM.ORD_CODE
+		GROUP BY
+				  STORE_ADDR,
+				  EMP_NAME;
+
+		  
   				
   				
   				
