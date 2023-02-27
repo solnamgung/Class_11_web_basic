391e7970b6eb31320ba6f877d16a53989d9388c8
user <nandaras0103@gmail.com>
2023-02-27 15:23:21 +0900

    combining arregates

--- WebContent/CodecademySQL/01_transformTable.sql
+++ WebContent/CodecademySQL/01_transformTable.sql
@@ -112,14 +112,138 @@
   EXCEPT
   SELECT category FROM new_products  ;
   	 
-
+ 
+ -- **********************************Aggregate functions revisited ***************************
+ 
+		-- NULL
+		-- We use the special keywords IS NULL or IS NOT NULL in the WHERE clause (= NULL does not work).
+		
+		SELECT
+		      COUNT(*)
+		FROM
+		    flights
+		WHERE
+		    1 = 1
+		AND arr_time IS NOT NULL
+		AND destination = 'ATL'; 
 		 
 		 
 		 
+		-- CASE WHEN (If ELSE is not included, the result will be NULL)
+		
+		 SELECT
+		    CASE
+		        WHEN elevation < 500 THEN 'Low'
+		        WHEN elevation BETWEEN 500 AND 1999 THEN 'Medium'
+		        WHEN elevation >= 2000 THEN 'High'
+		        ELSE 'Unknown'
+		    END AS elevation_tier
+		    , COUNT(*)
+		FROM airports
+		GROUP BY 1;
 		 
-		 
-		 
-		 
+		-- COUNT(CASE WHEN)
+		SELECT state, 
+		COUNT(CASE WHEN elevation < 1000 THEN 1 ELSE NULL END) as count_low_elevation_airports 
+		FROM airports 
+		GROUP BY state;
+		
+		
+	-- SUM(CASE WHEN)	
+		SELECT 
+		      origin,
+		      SUM(distance) AS TOTAL_FLIGHT_DISTANCE,
+		      SUM(CASE WHEN carrier = 'UA' THEN distance ELSE 0 END)
+		            AS total_united_flight_distance 
+		 FROM 
+		          flights
+	  GROUP BY
+	 	        origin;
+		        		
+	 	
+		SELECT
+		      origin,
+		      SUM(distance) AS total_flight_distance ,
+		    SUM(CASE WHEN carrier = 'DL' THEN distance ELSE 0 END) AS total_delta_flight_distance
+		FROM 
+		    flights
+		GROUP BY
+		        origin;
+		        
+	-- Combining aggregates
+		  SELECT origin, 
+			    (100.0*(sum(CASE WHEN carrier = 'UN' THEN distance ELSE 0 END))/sum(distance)) as percentage_flight_distance_from_united FROM flights 
+			GROUP BY origin;      
+					        
+	SELECT
+      origin,
+      (100.0 * 
+              SUM(CASE WHEN carrier = 'DL' THEN distance ELSE 0 END) / SUM(distance)) 
+              AS percentage_flight_distance_from_delta
+		FROM
+		    flights
+		GROUP BY origin;	        
+		        
+	
+		        
+		SELECT 
+      state,
+      100.0 * sum(CASE WHEN elevation >= 2000 THEN 1 ELSE 0 END) / count(*) AS percentage_high_elevation_airports
+      FROM
+          airports
+    GROUP BY
+          state;        
+		        
+		        
+		  
+ -- DATES
+	SELECT
+	      DATE(delivery_item),
+	      COUNT(*) as count_baked_goods
+	FROM
+	      baked_goods
+	GROUP BY
+	        DATE(delivery_time);	
+	        
+	        
+	 SELECT 
+      DATETIME(delivery_time, '+5 hours', '20 minutes', '2 days') as package_time
+	  FROM
+	      baked_goods;       
+	        
+	        
+	SELECT
+     	ROUND(distance, 2) AS distance_from_market
+	FROM
+     	bakeries;        
+	        
+	    -- Strings
+	    
+	SELECT first_name || ' ' || last_name as full_name
+		FROM bakeries;    
+	    
+	    SELECT REPLACE(ingredients,'enriched_','') as item_ingredients
+		FROM baked_goods;
+	    
+	    
+	        
+	        
+	        
+	        
+	        
+	                
+		        
+		        
+		        
+		
+		
+		
+		
+		
+		
+		
+		
+		
 		 
 		 
 		 
